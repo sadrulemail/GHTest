837a8e252a442d5899d571a061dd113e504797ef
sadrulemail <58506546+sadrulemail@users.noreply.github.com>
2021-02-22 20:31:50 +0600

    Add project files.

---
+++ GHTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GHTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
