3152790d0c58317e9940a301e4207f6dda0a99d0
sadrulemail <58506546+sadrulemail@users.noreply.github.com>
2021-02-23 08:41:07 +0600

    2nd Programmer changes

--- GHTest/Program.cs
+++ GHTest/Program.cs
@@ -9,6 +9,8 @@ namespace GHTest
             Console.WriteLine("Hello World!");
             Console.WriteLine("2nd Stage from Local!!");
              Console.WriteLine("3rd Stage from GitHub!!");
+            Console.WriteLine("2nd Programmer clone it, made change!!");
+
         }
     }
 }
