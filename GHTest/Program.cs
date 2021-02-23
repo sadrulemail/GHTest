8d28f165c1c0c90647017a463437958d71a0ab19
sadrulemail <58506546+sadrulemail@users.noreply.github.com>
2021-02-23 08:30:28 +0600

    2nd comit

--- GHTest/Program.cs
+++ GHTest/Program.cs
@@ -7,6 +7,7 @@ namespace GHTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("2nd Stage from Local!!");
         }
     }
 }
